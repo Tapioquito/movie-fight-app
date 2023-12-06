bfbe60f6d6ff1371463ef8b9c9ea2150cc31d36f
Caio Porto <92833972+Tapioquito@users.noreply.github.com>
2023-12-06 10:29:51 -0300

    added input search logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const fetchData = async () => {
-  const response = await axios.get();
+const apiKey = "269dfe5c";
+const fetchData = async (searchTerm) => {
+  const response = await axios.get(`http://www.omdbapi.com/`, {
+    params: {
+      apikey: apiKey,
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
 };
+
+const input = document.getElementsByTagName("input");
+
+input.addEventListener("input", (e) => {
+  fetchData(e.target.value);
+});
